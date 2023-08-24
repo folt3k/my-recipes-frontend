eebba5fc0e7977ba3c3b011631a7fb66112f0254
olga.jasionek <ogi22021996@gmail.com>
2023-08-24 09:43:39 +0200

    project-init, first config

---
+++ src/login/login.tsx
@@ -0,0 +1,18 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const Login = () => {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent></CardContent>
+      <CardActions>
+        <Button size='small'>Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Login;
